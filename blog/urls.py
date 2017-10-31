ee0b7b2db811a1e7edf98ec9df0052c65416a454
Corinne Bosley <corinne.e.bosley@gmail.com>
2017-10-31 12:38:14 +0000

    added some buttons for saving and editing and things

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ from . import views
 # after the root of the web server, it should show the user the post list.
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit')
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 
-# This is the root view
+# This is the root view.
 def post_list(request):
     # Filter the list of posts to find all the ones with a publish date and
     # then put them in ascending date order (you can reverse this with a
@@ -20,11 +21,40 @@ def post_list(request):
     # fields in the template.
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# This is for a single post in detail
+# This is for a single post in detail.
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+# This allows the user to create(?) a new blog.  Maybe...
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
 
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# This is for editing an existing post.
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
